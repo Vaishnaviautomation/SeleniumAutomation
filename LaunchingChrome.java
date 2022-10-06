3e1b1554c1c14f9665fd4b2abcc9a6178ac7870b
Vaishnavi <vaishnavinarwade98@gmail.com>
2022-10-06 13:36:26 +0530

    updated launchingCrome.jave

---
+++ LaunchingChrome.java
@@ -0,0 +1,18 @@
+package day1.selenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchingChrome {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", "D:\\workspace\\SeleniumPractice\\Executables\\chromedriver.exe");
+		
+		ChromeDriver driver= new ChromeDriver();
+		
+		driver.get("https://www.google.com");
+		
+		
+	}
+
+}
